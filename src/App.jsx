c62ebda9c5f026d197ba777601316844f00dbb38
Rafif <rafif24ti@mahasiswa.pcr.ac.id>
2026-06-08 07:29:57 +0700

    k

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./assets/tailwind.css";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Header from "./layouts/Header";
+import Sidebar from "./layouts/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import { Route, Routes } from "react-router-dom";
+import Products from "./pages/Produk";
+import ProductDetail from "./pages/ProductDetail";
+
+function App() {
+  // const ProductDetail = React.lazy(() => import("./pages/ProductDetail"));
+  return (
+    <div id="app-container" className="bg-gray-100 min-h-screen flex">
+      <Sidebar />
+      <div id="main-content" className="flex-1 p-4">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
